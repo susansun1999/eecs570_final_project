10f2acb921452f74c4c4e33deb7c87dd2f472916
zwtaoumich <zwtao@caen-vnc-vm16.engin.umich.edu>
2021-03-02 09:39:02 -0500

    init

---
+++ lab1/DVEfiles/session_2020-01-10T11_40_00.tcl
@@ -0,0 +1,318 @@
+# Begin_DVE_Session_Save_Info
+# DVE full session
+# Saved on Fri Jan 10 11:40:00 2020
+# Designs open: 1
+#   Sim: /afs/umich.edu/user/z/w/zwtao/Documents/470/Lab1/lab1/dve
+# Toplevel windows open: 1
+# 	TopLevel.1
+#   Schematic.1: testbench.tbp
+#   Source.1: testbench
+#   Group count = 2
+#   Group Group1 signal count = 8
+#   Group Group2 signal count = 7
+# End_DVE_Session_Save_Info
+
+# DVE version: N-2017.12-SP2-1_Full64
+# DVE build date: Jul 14 2018 20:58:30
+
+
+#<Session mode="Full" path="/afs/umich.edu/user/z/w/zwtao/Documents/470/Lab1/lab1/DVEfiles/session.tcl" type="Debug">
+
+gui_set_loading_session_type Post
+gui_continuetime_set
+
+# Close design
+if { [gui_sim_state -check active] } {
+    gui_sim_terminate
+}
+gui_close_db -all
+gui_expr_clear_all
+
+# Close all windows
+gui_close_window -type Console
+gui_close_window -type Wave
+gui_close_window -type Source
+gui_close_window -type Schematic
+gui_close_window -type Data
+gui_close_window -type DriverLoad
+gui_close_window -type List
+gui_close_window -type Memory
+gui_close_window -type HSPane
+gui_close_window -type DLPane
+gui_close_window -type Assertion
+gui_close_window -type CovHier
+gui_close_window -type CoverageTable
+gui_close_window -type CoverageMap
+gui_close_window -type CovDetail
+gui_close_window -type Local
+gui_close_window -type Stack
+gui_close_window -type Watch
+gui_close_window -type Group
+gui_close_window -type Transaction
+
+
+
+# Application preferences
+gui_set_pref_value -key app_default_font -value {Helvetica,10,-1,5,50,0,0,0,0,0}
+gui_src_preferences -tabstop 8 -maxbits 24 -windownumber 1
+#<WindowLayout>
+
+# DVE top-level session
+
+
+# Create and position top-level window: TopLevel.1
+
+if {![gui_exist_window -window TopLevel.1]} {
+    set TopLevel.1 [ gui_create_window -type TopLevel \
+       -icon $::env(DVE)/auxx/gui/images/toolbars/dvewin.xpm] 
+} else { 
+    set TopLevel.1 TopLevel.1
+}
+gui_show_window -window ${TopLevel.1} -show_state normal -rect {{0 66} {1278 1405}}
+
+# ToolBar settings
+gui_set_toolbar_attributes -toolbar {TimeOperations} -dock_state top
+gui_set_toolbar_attributes -toolbar {TimeOperations} -offset 0
+gui_show_toolbar -toolbar {TimeOperations}
+gui_hide_toolbar -toolbar {&File}
+gui_set_toolbar_attributes -toolbar {&Edit} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Edit} -offset 0
+gui_show_toolbar -toolbar {&Edit}
+gui_hide_toolbar -toolbar {CopyPaste}
+gui_set_toolbar_attributes -toolbar {&Trace} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Trace} -offset 0
+gui_show_toolbar -toolbar {&Trace}
+gui_hide_toolbar -toolbar {TraceInstance}
+gui_hide_toolbar -toolbar {BackTrace}
+gui_set_toolbar_attributes -toolbar {&Scope} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Scope} -offset 0
+gui_show_toolbar -toolbar {&Scope}
+gui_set_toolbar_attributes -toolbar {&Window} -dock_state top
+gui_set_toolbar_attributes -toolbar {&Window} -offset 0
+gui_show_toolbar -toolbar {&Window}
+gui_set_toolbar_attributes -toolbar {Signal} -dock_state top
+gui_set_toolbar_attributes -toolbar {Signal} -offset 0
+gui_show_toolbar -toolbar {Signal}
+gui_set_toolbar_attributes -toolbar {Zoom} -dock_state top
+gui_set_toolbar_attributes -toolbar {Zoom} -offset 0
+gui_show_toolbar -toolbar {Zoom}
+gui_set_toolbar_attributes -toolbar {Zoom And Pan History} -dock_state top
+gui_set_toolbar_attributes -toolbar {Zoom And Pan History} -offset 0
+gui_show_toolbar -toolbar {Zoom And Pan History}
+gui_set_toolbar_attributes -toolbar {Grid} -dock_state top
+gui_set_toolbar_attributes -toolbar {Grid} -offset 0
+gui_show_toolbar -toolbar {Grid}
+gui_set_toolbar_attributes -toolbar {Simulator} -dock_state top
+gui_set_toolbar_attributes -toolbar {Simulator} -offset 0
+gui_show_toolbar -toolbar {Simulator}
+gui_set_toolbar_attributes -toolbar {Interactive Rewind} -dock_state top
+gui_set_toolbar_attributes -toolbar {Interactive Rewind} -offset 0
+gui_show_toolbar -toolbar {Interactive Rewind}
+gui_set_toolbar_attributes -toolbar {Testbench} -dock_state top
+gui_set_toolbar_attributes -toolbar {Testbench} -offset 0
+gui_show_toolbar -toolbar {Testbench}
+
+# End ToolBar settings
+
+# Docked window settings
+set HSPane.1 [gui_create_window -type HSPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 169]
+catch { set Hier.1 [gui_share_window -id ${HSPane.1} -type Hier] }
+gui_set_window_pref_key -window ${HSPane.1} -key dock_width -value_type integer -value 169
+gui_set_window_pref_key -window ${HSPane.1} -key dock_height -value_type integer -value -1
+gui_set_window_pref_key -window ${HSPane.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${HSPane.1} {{left 0} {top 0} {width 168} {height 1062} {dock_state left} {dock_on_new_line true} {child_hier_colhier 140} {child_hier_coltype 100} {child_hier_colpd 0} {child_hier_col1 0} {child_hier_col2 1} {child_hier_col3 -1}}
+set DLPane.1 [gui_create_window -type DLPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 398]
+catch { set Data.1 [gui_share_window -id ${DLPane.1} -type Data] }
+gui_set_window_pref_key -window ${DLPane.1} -key dock_width -value_type integer -value 398
+gui_set_window_pref_key -window ${DLPane.1} -key dock_height -value_type integer -value 382
+gui_set_window_pref_key -window ${DLPane.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${DLPane.1} {{left 0} {top 0} {width 397} {height 1062} {dock_state left} {dock_on_new_line true} {child_data_colvariable 159} {child_data_colvalue 119} {child_data_coltype 110} {child_data_col1 0} {child_data_col2 1} {child_data_col3 2}}
+set Console.1 [gui_create_window -type Console -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line true -dock_extent 179]
+gui_set_window_pref_key -window ${Console.1} -key dock_width -value_type integer -value -1
+gui_set_window_pref_key -window ${Console.1} -key dock_height -value_type integer -value 179
+gui_set_window_pref_key -window ${Console.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${Console.1} {{left 0} {top 0} {width 271} {height 178} {dock_state bottom} {dock_on_new_line true}}
+set DriverLoad.1 [gui_create_window -type DriverLoad -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line false -dock_extent 179]
+gui_set_window_pref_key -window ${DriverLoad.1} -key dock_width -value_type integer -value 150
+gui_set_window_pref_key -window ${DriverLoad.1} -key dock_height -value_type integer -value 179
+gui_set_window_pref_key -window ${DriverLoad.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${DriverLoad.1} {{left 0} {top 0} {width 1006} {height 178} {dock_state bottom} {dock_on_new_line false}}
+#### Start - Readjusting docked view's offset / size
+set dockAreaList { top left right bottom }
+foreach dockArea $dockAreaList {
+  set viewList [gui_ekki_get_window_ids -active_parent -dock_area $dockArea]
+  foreach view $viewList {
+      if {[lsearch -exact [gui_get_window_pref_keys -window $view] dock_width] != -1} {
+        set dockWidth [gui_get_window_pref_value -window $view -key dock_width]
+        set dockHeight [gui_get_window_pref_value -window $view -key dock_height]
+        set offset [gui_get_window_pref_value -window $view -key dock_offset]
+        if { [string equal "top" $dockArea] || [string equal "bottom" $dockArea]} {
+          gui_set_window_attributes -window $view -dock_offset $offset -width $dockWidth
+        } else {
+          gui_set_window_attributes -window $view -dock_offset $offset -height $dockHeight
+        }
+      }
+  }
+}
+#### End - Readjusting docked view's offset / size
+gui_sync_global -id ${TopLevel.1} -option true
+
+# MDI window settings
+set Source.1 [gui_create_window -type {Source}  -parent ${TopLevel.1}]
+gui_show_window -window ${Source.1} -show_state maximized
+gui_update_layout -id ${Source.1} {{show_state maximized} {dock_state undocked} {dock_on_new_line false}}
+gui_use_schematics
+set Schematic.1 [gui_create_window -type {Schematic}  -parent ${TopLevel.1} -defer_create_taskbar_icon]
+set setting [::Misc::Setting::create -array DveSchematicSettings]
+Misc::init_window $setting ${Schematic.1}
+::Misc::exec_method -window ${Schematic.1} -method captionCmd
+gui_add_icon_to_taskbar -window ${Schematic.1}
+gui_show_window -window ${Schematic.1} -show_state maximized
+gui_update_layout -id ${Schematic.1} {{show_state maximized} {dock_state undocked} {dock_on_new_line false}}
+
+# End MDI window settings
+
+gui_set_env TOPLEVELS::TARGET_FRAME(Source) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(Schematic) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(PathSchematic) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(Wave) none
+gui_set_env TOPLEVELS::TARGET_FRAME(List) none
+gui_set_env TOPLEVELS::TARGET_FRAME(Memory) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(DriverLoad) none
+gui_update_statusbar_target_frame ${TopLevel.1}
+
+#</WindowLayout>
+
+#<Database>
+
+# DVE Open design session: 
+
+if { [llength [lindex [gui_get_db -design Sim] 0]] == 0 } {
+gui_set_env SIMSETUP::SIMARGS {{-ucligui +vc +define+ +memcbk}}
+gui_set_env SIMSETUP::SIMEXE {/afs/umich.edu/user/z/w/zwtao/Documents/470/Lab1/lab1/dve}
+gui_set_env SIMSETUP::ALLOW_POLL {0}
+if { ![gui_is_db_opened -db {/afs/umich.edu/user/z/w/zwtao/Documents/470/Lab1/lab1/dve}] } {
+gui_sim_run Ucli -exe dve -args {-ucligui +vc +define+ +memcbk} -dir /afs/umich.edu/user/z/w/zwtao/Documents/470/Lab1/lab1 -nosource
+}
+}
+if { ![gui_sim_state -check active] } {error "Simulator did not start correctly" error}
+gui_set_precision 1s
+gui_set_time_units 1s
+#</Database>
+
+# DVE Global setting session: 
+
+
+# Global: Breakpoints
+
+# Global: Bus
+
+# Global: Expressions
+
+# Global: Signal Time Shift
+
+# Global: Signal Compare
+
+# Global: Signal Groups
+gui_load_child_values {testbench.tbp}
+
+
+set _session_group_1 Group1
+gui_sg_create "$_session_group_1"
+set Group1 "$_session_group_1"
+
+gui_sg_addsignal -group "$_session_group_1" { testbench.tbp.clock testbench.tbp.reset testbench.tbp.taken testbench.tbp.transition testbench.tbp.state testbench.tbp.next_state {testbench.tbp.state[0]} {testbench.tbp.state[1]} }
+gui_set_radix -radix {decimal} -signals {Sim:testbench.tbp.state}
+gui_set_radix -radix {unsigned} -signals {Sim:testbench.tbp.state}
+
+set _session_group_2 Group2
+gui_sg_create "$_session_group_2"
+set Group2 "$_session_group_2"
+
+gui_sg_addsignal -group "$_session_group_2" { testbench.tbp.clock testbench.tbp.reset testbench.tbp.taken testbench.tbp.transition testbench.tbp.prediction testbench.tbp.state testbench.tbp.next_state }
+gui_set_radix -radix {decimal} -signals {Sim:testbench.tbp.state}
+gui_set_radix -radix {unsigned} -signals {Sim:testbench.tbp.state}
+
+# Global: Highlighting
+gui_highlight_signals -color #00ff00 {{testbench.tbp.state[1]} {testbench.tbp.state[1:0]}}
+
+# Global: Stack
+gui_change_stack_mode -mode list
+
+# Post database loading setting...
+
+# Restore C1 time
+gui_set_time -C1_only 100
+
+
+
+# Save global setting...
+
+# Wave/List view global setting
+gui_cov_show_value -switch false
+
+# Close all empty TopLevel windows
+foreach __top [gui_ekki_get_window_ids -type TopLevel] {
+    if { [llength [gui_ekki_get_window_ids -parent $__top]] == 0} {
+        gui_close_window -window $__top
+    }
+}
+gui_set_loading_session_type noSession
+# DVE View/pane content session: 
+
+
+# Hier 'Hier.1'
+gui_show_window -window ${Hier.1}
+gui_list_set_filter -id ${Hier.1} -list { {Package 1} {All 0} {Process 1} {VirtPowSwitch 0} {UnnamedProcess 1} {UDP 0} {Function 1} {Block 1} {SrsnAndSpaCell 0} {OVA Unit 1} {LeafScCell 1} {LeafVlgCell 1} {Interface 1} {LeafVhdCell 1} {$unit 1} {NamedBlock 1} {Task 1} {VlgPackage 1} {ClassDef 1} {VirtIsoCell 0} }
+gui_list_set_filter -id ${Hier.1} -text {*}
+gui_hier_list_init -id ${Hier.1}
+gui_change_design -id ${Hier.1} -design Sim
+catch {gui_list_select -id ${Hier.1} {testbench}}
+gui_view_scroll -id ${Hier.1} -vertical -set 0
+gui_view_scroll -id ${Hier.1} -horizontal -set 1
+
+# Data 'Data.1'
+gui_list_set_filter -id ${Data.1} -list { {Buffer 1} {Input 1} {Others 1} {Linkage 1} {Output 1} {LowPower 1} {Parameter 1} {All 1} {Aggregate 1} {LibBaseMember 1} {Event 1} {Assertion 1} {Constant 1} {Interface 1} {BaseMembers 1} {Signal 1} {$unit 1} {Inout 1} {Variable 1} }
+gui_list_set_filter -id ${Data.1} -text {*}
+gui_list_show_data -id ${Data.1} {testbench}
+gui_show_window -window ${Data.1}
+catch { gui_list_select -id ${Data.1} {testbench.clock testbench.reset testbench.taken testbench.transition testbench.bigsignal testbench.prediction }}
+gui_view_scroll -id ${Data.1} -vertical -set 0
+gui_view_scroll -id ${Data.1} -horizontal -set 0
+gui_view_scroll -id ${Hier.1} -vertical -set 0
+gui_view_scroll -id ${Hier.1} -horizontal -set 1
+
+# DriverLoad 'DriverLoad.1'
+gui_get_drivers -session -id ${DriverLoad.1} -signal testbench.tbp.clock -time 70 -starttime 70
+gui_get_drivers -session -id ${DriverLoad.1} -signal testbench.tbp.reset -time 20 -starttime 70
+gui_get_drivers -session -id ${DriverLoad.1} -signal testbench.tbp.transition -time 70 -starttime 70
+gui_get_drivers -session -id ${DriverLoad.1} -signal testbench.tbp.clock -time 100 -starttime 100
+gui_get_drivers -session -id ${DriverLoad.1} -signal testbench.clock -time 90 -starttime 90
+gui_get_drivers -session -id ${DriverLoad.1} -signal testbench.tbp.clock -time 75 -starttime 75
+gui_get_drivers -session -id ${DriverLoad.1} -signal testbench.tbp.clock -time 15 -starttime 15
+gui_get_drivers -session -id ${DriverLoad.1} -signal testbench.tbp.prediction -time 6 -starttime 25
+
+# Source 'Source.1'
+gui_src_value_annotate -id ${Source.1} -switch false
+gui_set_env TOGGLE::VALUEANNOTATE 0
+gui_open_source -id ${Source.1}  -replace -active testbench /afs/umich.edu/user/z/w/zwtao/Documents/470/Lab1/lab1/tut_test.v
+gui_view_scroll -id ${Source.1} -vertical -set 0
+gui_src_set_reusable -id ${Source.1}
+
+# View 'Schematic.1'
+gui_use_schematics
+
+# Create schematic window 'Schematic.1'
+gui_sch_show -window ${Schematic.1} -name testbench.tbp
+gui_show_pin_value_annotate [gui_window_hier_name -window ${Schematic.1}]
+gui_zoom -window ${Schematic.1} -rect { {-40067 -40392} {82085 23843} }
+
+
+# Restore toplevel window zorder
+# The toplevel window could be closed if it has no view/pane
+if {[gui_exist_window -window ${TopLevel.1}]} {
+	gui_set_active_window -window ${TopLevel.1}
+	gui_set_active_window -window ${Source.1}
+}
+#</Session>
+
